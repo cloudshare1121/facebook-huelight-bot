a55abc8b342c4076f92dd271d2b796493e935ee4
cloudshare1121 <31614023+cloudshare1121@users.noreply.github.com>
2017-09-15 11:33:53 +0200

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -6,39 +6,37 @@ let salesforce = require('./salesforce'),
 
 exports.allon = (sender, values) => {
     let hueId = values[1];
-    console.log('DEBUG: values: '+values[0]);
-     console.log('DEBUG: values.length: '+values.length);
-    console.log('DEBUG: hueid: '+hueId);
-    messenger.getUserInfo(sender).then(response => {
-        console.log('DEBUG: response: '+response);
-        console.log('DEBUG: sender: '+sender);
-        salesforce.createHueRequest(response.first_name + " " + response.first_name,sender, response.message, response.current_location).then(() => {
+    let hueMessage = values[0];
+    console.log('DEBUG: values: '+values[0]);    
+    messenger.getUserInfo(sender).then(response => {       
+        salesforce.createHueRequest(response.first_name + " " + response.first_name,sender, hueMessage, response.current_location).then(() => {
             messenger.send({text: `Okay, ${response.first_name}, turning your lights on.`}, sender);
         });
     });
 };
 
 exports.alloff = (sender, values) => {
-    
+    let hueMessage = values[0];
     messenger.getUserInfo(sender).then(response => {
-        salesforce.createHueRequest(response.first_name + " " + response.first_name, sender).then(() => {
+        salesforce.createHueRequest(response.first_name + " " + response.first_name,sender, hueMessage, response.current_location).then(() => {
             messenger.send({text: `Okay, ${response.first_name}, turning your lights off.`}, sender);
         });
     });
 };
 
 exports.allred = (sender, values) => {
-    
+    let hueMessage = values[0];
     messenger.getUserInfo(sender).then(response => {
-        salesforce.createHueRequest(response.first_name + " " + response.first_name, sender).then(() => {
+        salesforce.createHueRequest(response.first_name + " " + response.first_name,sender, hueMessage, response.current_location).then(() => {
             messenger.send({text: `Wow! ${response.first_name}, that's romantic and cosy :) There you go, all red for you.`}, sender);
         });
     });
 };
 
-exports.random = (sender, values) => {    
+exports.random = (sender, values) => {   
+    let hueMessage = values[0];
     messenger.getUserInfo(sender).then(response => {
-        salesforce.createHueRequest(response.first_name + " " + response.first_name, sender).then(() => {
+        salesforce.createHueRequest(response.first_name + " " + response.first_name,sender, hueMessage, response.current_location).then(() => {
             messenger.send({text: `Thanks, ${response.first_name}. Finally, I get to choose the mood for you ;)`}, sender);
         });
     });
