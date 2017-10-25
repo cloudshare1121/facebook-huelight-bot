8cb8b041b895f9f08a47312572c087834cb78f48
cloudshare1121 <31614023+cloudshare1121@users.noreply.github.com>
2017-10-25 15:51:04 +0200

    Update formatter.js

--- modules/formatter.js
+++ modules/formatter.js
@@ -8,27 +8,29 @@ exports.formatLights = lights => {
     lights.forEach(lamp => {
         var device = lamp.get("Device_Type__c");
         console.log("Device_Type__c: "+device)
-            elements.push({
-                title: lamp.get("Title__c"),                
-                "image_url": lamp.get("Picture__c"),
-                "buttons": [
-                    {
-                        "type": "postback",
-                        "title": "Turn On",
-                        "payload": "allon," + lamp.getId()
-                    },
-                    {
-                        "type": "postback",
-                        "title": "Turn Off",
-                        "payload": "alloff," + lamp.getId()
-                    }, 
-                    {
-                        "type": "postback",
-                        "title": "Turn Red",
-                        "payload": "allred," + lamp.getId()
-                    },
-                ]
-            })
+        if(device=="Lights"){
+                elements.push({
+                    title: lamp.get("Title__c"),                
+                    "image_url": lamp.get("Picture__c"),
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Turn On",
+                            "payload": "allon," + lamp.getId()
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Turn Off",
+                            "payload": "alloff," + lamp.getId()
+                        }, 
+                        {
+                            "type": "postback",
+                            "title": "Turn Red",
+                            "payload": "allred," + lamp.getId()
+                        },
+                    ]
+                })
+            }
         }
     );
     return {
